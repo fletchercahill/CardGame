6cb7e45537ebf7dc8415055f5b4945efa5df56d9
Fletcher Cahill <9fletch88@gmail.com>
2026-02-23 14:23:01 -0800

    Full working product with end game handling.

--- src/main/java/GameView.java
+++ src/main/java/GameView.java
@@ -28,17 +28,19 @@ public class GameView extends JFrame{
         }
 
     }
-    private void drawPlayerWin(Graphics g){
-        g.setFont(new Font("Georgia", Font.BOLD, 60));
-        g.drawString("You win!", 30, 150);
-    }
-    private void drawCpuWin(Graphics g){
-        g.setFont(new Font("Georgia", Font.BOLD, 60));
-        g.drawString("CPU wins!", 30, 150);
-    }
-    private void drawTie(Graphics g){
-        g.setFont(new Font("Georgia", Font.BOLD, 60));
-        g.drawString("Tie", 30, 150);
+    private void handleWin(Graphics g){
+        if (backend.getWinner() == 2){
+            g.setFont(new Font("Georgia", Font.BOLD, 60));
+            g.drawString("CPU wins!", 30, 150);
+        }
+        else if (backend.getWinner() == 1){
+            g.setFont(new Font("Georgia", Font.BOLD, 60));
+            g.drawString("You win!", 30, 150);
+        }
+        else if (backend.getWinner() == 3){
+            g.setFont(new Font("Georgia", Font.BOLD, 60));
+            g.drawString("Tie", 30, 150);
+        }
     }
     private void drawCpuCards(Graphics g){
         int initialX = 370;
@@ -60,6 +62,28 @@ public class GameView extends JFrame{
             x+=spacing;
         }
     }
+    private void endGame(Graphics g){
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        g.setColor(Color.ORANGE);
+        g.setFont(new Font("Georgia", Font.BOLD, 100));
+        if (backend.getCpuMoney() > 0){
+            g.drawString("Thanks for Playing", 30, 200);
+        }
+        else{
+            g.drawString("YOU WIN!", 80, 200);
+        }
+    }
+    private void drawInstructions(Graphics g){
+        g.setFont(new Font("Georgia", Font.BOLD, 30));
+        g.drawString("INSTRUCTIONS", 25, 655);
+        g.setFont(new Font("Georgia", Font.BOLD, 18));
+        g.drawString("Welcome to Poker Reimagined!", 25, 700);
+        g.drawString("You have one opportuntity to bet.", 25, 720);
+        g.drawString("Best poker hand wins.", 25, 740);
+        g.drawString("Place a bet to get started!", 25, 760);
+        g.setFont(new Font("SansSerif", Font.BOLD, 50));
+    }
     public void paint (Graphics g){
         super.paint(g);
         g.drawImage(bgImage, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, this);
@@ -70,14 +94,7 @@ public class GameView extends JFrame{
         g.drawImage(chips, 600, 50, 100, 100, this);
         g.fillOval(350, 50, 220, 100);
         g.setColor(Color.BLACK);
-        g.setFont(new Font("Georgia", Font.BOLD, 30));
-        g.drawString("INSTRUCTIONS", 25, 655);
-        g.setFont(new Font("Georgia", Font.BOLD, 18));
-        g.drawString("Welcome to Poker Reimagined!", 25, 700);
-        g.drawString("You have one opportuntity to bet.", 25, 720);
-        g.drawString("Best poker hand wins.", 25, 740);
-        g.drawString("Place a bet to get started!", 25, 760);
-        g.setFont(new Font("SansSerif", Font.BOLD, 50));
+        drawInstructions(g);
         g.drawString(Integer.toString(backend.getMoney()), 410 , 720);
         g.drawString(Integer.toString(backend.getCpuMoney()), 410 , 115);
         g.drawString("CPU", 750, 125);
@@ -85,21 +102,10 @@ public class GameView extends JFrame{
         drawPlayerCards(g);
         drawCpuCards(g);
         drawTableCards(g);
-        if (backend.getWinner() == 2){
-            drawCpuWin(g);
-            System.out.println("5");
-
+        handleWin(g);
+        if (backend.getCpuMoney() <= 0 || backend.getMoney() <= 0){
+            endGame(g);
         }
-        else if (backend.getWinner() == 1){
-            drawPlayerWin(g);
-            System.out.println("6");
-
-        }
-        else if (backend.getWinner() == 3){
-            drawTie(g);
-        }
-
-
     }
 
 }
